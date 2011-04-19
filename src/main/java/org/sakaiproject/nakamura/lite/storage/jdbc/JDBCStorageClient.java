6ec2692263cf0dca4a7c92adc1efa49e1627dd08
Zach A. Thomas <zach@aeroplanesoftware.com>
2011-04-19 11:06:55 -0500

    NOJIRA cleaning up a disused import statement.

--- src/main/java/org/sakaiproject/nakamura/lite/storage/jdbc/JDBCStorageClient.java
+++ src/main/java/org/sakaiproject/nakamura/lite/storage/jdbc/JDBCStorageClient.java
@@ -55,7 +55,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.MessageFormat;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
