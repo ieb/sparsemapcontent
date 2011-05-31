b549870e78a0fb1ce53a9e64963e23e341f8c094
Ian Boston <ieb@tfd.co.uk>
2011-05-31 16:10:08 +0100

    KERN-1915 Added support for PostgreSQL, unit tests configured to work against v9, but will probably work for v8

---
+++ src/main/resources/org/sakaiproject/nakamura/lite/storage/jdbc/config/client.PostgreSQL.sql
@@ -0,0 +1,81 @@
+# SQL statements of the form key[.keyspace.columnfamily.[rowID0-2]]
+# the based key should always be present
+# the keyspace.columnfamily selectors are used to shard the column family (optional)
+# the rowID0-2 is to shard on rowID, you can selectively shard hot rowID areas.
+# If sharding ensure that any exiting data is migrated (using SQL DML) and that the finder statements are adjusted to incorporate the shards (warning, might be hard)
+# Indexer statements
+delete-string-row = delete from css where rid = ?
+delete-string-row.n.ac = delete from ac_css where rid = ?
+delete-string-row.n.au = delete from au_css where rid = ?
+delete-string-row.n.cn = delete from cn_css where rid = ?
+select-string-row = select cid, v from css where rid = ?
+select-string-row.n.ac = select cid, v from ac_css where rid = ?
+select-string-row.n.au = select cid, v from au_css where rid = ?
+select-string-row.n.cn = select cid, v from cn_css where rid = ?
+insert-string-column = insert into css (v, rid, cid) values ( ?, ?, ? )
+insert-string-column.n.ac = insert into ac_css (v, rid, cid) values ( ?, ?, ? )
+insert-string-column.n.au = insert into au_css (v, rid, cid) values ( ?, ?, ? )
+insert-string-column.n.cn = insert into cn_css (v, rid, cid) values ( ?, ?, ? )
+update-string-column = update css set v = ?  where rid = ? and cid = ?
+update-string-column.n.ac = update ac_css set v = ?  where rid = ? and cid = ?
+update-string-column.n.au = update au_css set v = ?  where rid = ? and cid = ?
+update-string-column.n.cn = update cn_css set v = ?  where rid = ? and cid = ?
+remove-string-column = delete from css where rid = ? and cid = ?
+remove-string-column.n.ac = delete from ac_css where rid = ? and cid = ?
+remove-string-column.n.au = delete from au_css where rid = ? and cid = ?
+remove-string-column.n.cn = delete from cn_css where rid = ? and cid = ?
+# Example of a sharded query, rowIDs starting with x will use this
+### remove-string-column.n.cn._X = delete from cn_css_X where rid = ? and cid = ?
+
+# base statement with paging ; table join ; where clause ; where clause for sort field (if needed) ; order by clause
+find.n.au = select a.rid, a.cid, a.v from au_css where {1} 1 = 1 {2} limit {3,number,#} offset {4,number,#};, au_css {0} ; {0}.cid = ? and {0}.v = ? and {0}.rid = a.rid ; {0}.cid = ? and {0}.rid = a.rid ; order by {0}.v {1}
+find.n.ac = select a.rid, a.cid, a.v from ac_css where {1} 1 = 1 {2} limit {3,number,#} offset {4,number,#};, ac_css {0} ; {0}.cid = ? and {0}.v = ? and {0}.rid = a.rid ; {0}.cid = ? and {0}.rid = a.rid ; order by {0}.v {1}
+find.n.cn = select a.rid, a.cid, a.v from cn_css where {1} 1 = 1 {2} limit {3,number,#} offset {4,number,#};, cn_css {0} ; {0}.cid = ? and {0}.v = ? and {0}.rid = a.rid ; {0}.cid = ? and {0}.rid = a.rid ; order by {0}.v {1}
+select-index-columns = select cid from index_cols
+
+block-select-row = select b from css_b where rid = ?
+block-delete-row = delete from css_b where rid = ?
+block-insert-row = insert into css_b (rid,b) values (?, ?)
+block-update-row = update css_b set b = ? where rid = ?
+
+block-select-row.n.ac = select b from ac_css_b where rid = ?
+block-delete-row.n.ac = delete from ac_css_b where rid = ?
+block-insert-row.n.ac = insert into ac_css_b (rid,b) values (?, ?)
+block-update-row.n.ac = update ac_css_b set b = ? where rid = ?
+
+block-select-row.n.cn = select b from cn_css_b where rid = ?
+block-delete-row.n.cn = delete from cn_css_b where rid = ?
+block-insert-row.n.cn = insert into cn_css_b (rid,b) values (?, ?)
+block-update-row.n.cn = update cn_css_b set b = ? where rid = ?
+
+block-select-row.n.au = select b from au_css_b where rid = ?
+block-delete-row.n.au = delete from au_css_b where rid = ?
+block-insert-row.n.au = insert into au_css_b (rid,b) values (?, ?)
+block-update-row.n.au = update au_css_b set b = ? where rid = ?
+
+# base statement with paging ; table join ; where clause ; where clause for sort field (if needed) ; order by clause; sort field column( if needed)
+## the subselect in the paging statement is required by Oracle to do paging. http://www.oracle.com/technetwork/issue-archive/2006/06-sep/o56asktom-086197.html
+block-find = select distinct a.rid {5} from css a {0} where {1} 1 = 1 {2} limit {3,number,#} offset {4,number,#};, css {0} ; {0}.cid = ? and {0}.v = ? and {0}.rid = a.rid ; {0}.cid = ? and {0}.rid = a.rid ; order by {0}.v {1} ;, {0}.v
+block-find.n.au = select distinct a.rid {5} from au_css a {0} where {1} 1 = 1 {2} limit {3,number,#} offset {4,number,#};, au_css {0} ; {0}.cid = ? and {0}.v = ? and {0}.rid = a.rid ; {0}.cid = ? and {0}.rid = a.rid ; order by {0}.v {1} ;, {0}.v
+block-find.n.cn = select distinct a.rid {5} from cn_css a {0} where {1} 1 = 1 {2} limit {3,number,#} offset {4,number,#};, cn_css {0} ; {0}.cid = ? and {0}.v = ? and {0}.rid = a.rid ; {0}.cid = ? and {0}.rid = a.rid ; order by {0}.v {1} ;, {0}.v
+block-find.n.ac = select distinct a.rid {5} from ac_css a {0} where {1} 1 = 1 {2} limit {3,number,#} offset {4,number,#};, ac_css {0} ; {0}.cid = ? and {0}.v = ? and {0}.rid = a.rid ; {0}.cid = ? and {0}.rid = a.rid ; order by {0}.v {1} ;, {0}.v
+
+
+# statement to validate the connection
+validate = select 1
+
+# What type of rowID has should be used. Must be non colliding (reasonable probability), cant be changed once set without data migration.
+# SHA-1 has a 1:10E14 probability of collision, so IMVHO is Ok here. Do not use MD5, it will collide.
+rowid-hash = SHA1
+
+# statement to check that the schema exists
+check-schema = select count(*) from css
+
+# Use batch Inserts means that update operations will be performed as batches rather than single SQL statements. This only really effects the update of
+# Index tables and not the content store but it will reduce the number of SQL operations where more than one field is indexed per content item.
+use-batch-inserts = 1
+
+# this property indicates which version of the JRE your JDBC driver targets
+# e.g. the driver for Postgres 9 does not support JDBC methods introduced in JRE 1.6
+jdbc-support-level = 1.5
+
---
+++ src/test/java/org/sakaiproject/nakamura/lite/jdbc/postgresql/PostgreSQLSetup.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The SF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.sakaiproject.nakamura.lite.jdbc.postgresql;
+
+import com.google.common.collect.ImmutableMap;
+
+import org.sakaiproject.nakamura.lite.storage.jdbc.JDBCStorageClientPool;
+
+public class PostgreSQLSetup {
+
+    private static JDBCStorageClientPool clientPool = createClientPool();
+
+    public synchronized static JDBCStorageClientPool createClientPool() {
+        try {
+            JDBCStorageClientPool connectionPool = new JDBCStorageClientPool();
+            connectionPool
+                    .activate(ImmutableMap
+                            .of(JDBCStorageClientPool.CONNECTION_URL,
+                                    (Object) "jdbc:postgresql://localhost/nak",
+                                    JDBCStorageClientPool.JDBC_DRIVER, "org.postgresql.Driver",
+                                    "username", "nakamura", "password", "nakamura"));
+            return connectionPool;
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
+    public static JDBCStorageClientPool getClientPool() {
+        return clientPool;
+    }
+}
