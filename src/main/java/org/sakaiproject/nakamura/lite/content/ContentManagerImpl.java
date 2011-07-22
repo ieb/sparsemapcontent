51db8cd791bcc32ca04168b2f57f6f341a7de937
Ian Boston <ieb@tfd.co.uk>
2011-07-22 12:19:25 +0100

    Fixed potential NPE

--- src/main/java/org/sakaiproject/nakamura/lite/content/ContentManagerImpl.java
+++ src/main/java/org/sakaiproject/nakamura/lite/content/ContentManagerImpl.java
@@ -422,7 +422,7 @@ public class ContentManagerImpl extends CachingManager implements ContentManager
         checkOpen();
         accessControlManager.check(Security.ZONE_CONTENT, path, Permissions.CAN_WRITE);
         Map<String, Object> structure = getCached(keySpace, contentColumnFamily, path);
-        if ( structure.size() == 0 || structure == null ) {
+        if ( structure == null || structure.size() == 0 ) {
             Content content = new Content(path,null);
             update(content);
             structure = getCached(keySpace, contentColumnFamily, path);
