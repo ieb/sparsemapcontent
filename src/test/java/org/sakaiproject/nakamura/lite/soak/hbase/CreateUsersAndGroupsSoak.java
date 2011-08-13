6adcf72dcaf0be2165a09ca5d5ee9bd59dded604
Aadish Kotwal <kotwal.aadish@gmail.com>
2011-08-13 12:51:06 +0530

    Tests for HBase driver Soak tests for HBase driver

---
+++ src/test/java/org/sakaiproject/nakamura/lite/soak/hbase/CreateUsersAndGroupsSoak.java
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Sakai Foundation (SF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The SF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.sakaiproject.nakamura.lite.soak.hbase;
+
+import com.google.common.collect.ImmutableMap;
+
+import org.sakaiproject.nakamura.api.lite.ClientPoolException;
+import org.sakaiproject.nakamura.api.lite.StorageClientException;
+import org.sakaiproject.nakamura.api.lite.StorageClientUtils;
+import org.sakaiproject.nakamura.api.lite.accesscontrol.AccessDeniedException;
+import org.sakaiproject.nakamura.lite.soak.AbstractSoakController;
+import org.sakaiproject.nakamura.lite.soak.authorizable.CreateUsersAndGroupsClient;
+import org.sakaiproject.nakamura.lite.storage.StorageClientPool;
+import org.sakaiproject.nakamura.lite.storage.hbase.HBaseStorageClientPool;
+
+public class CreateUsersAndGroupsSoak extends AbstractSoakController {
+    private int totalUsers;
+    private StorageClientPool connectionPool;
+
+    public CreateUsersAndGroupsSoak(int totalUsers, StorageClientPool connectionPool) {
+        super(totalUsers);
+        this.connectionPool = connectionPool;
+        this.totalUsers = totalUsers;
+    }
+
+    protected Runnable getRunnable(int nthreads) throws ClientPoolException,
+            StorageClientException, AccessDeniedException {
+        int usersPerThread = totalUsers / nthreads;
+        return new CreateUsersAndGroupsClient(usersPerThread, connectionPool);
+    }
+
+    public static void main(String[] argv) throws ClientPoolException, StorageClientException,
+            AccessDeniedException, ClassNotFoundException {
+
+        int totalUsers = 1000;
+        int nthreads = 10;
+
+        if (argv.length > 0) {
+            nthreads = StorageClientUtils.getSetting(Integer.valueOf(argv[0]), nthreads);
+        }
+        if (argv.length > 1) {
+            totalUsers = StorageClientUtils.getSetting(Integer.valueOf(argv[1]), totalUsers);
+        }
+
+        CreateUsersAndGroupsSoak createUsersAndGroupsSoak = new CreateUsersAndGroupsSoak(
+                totalUsers, getConnectionPool());
+        createUsersAndGroupsSoak.launchSoak(nthreads);
+    }
+
+    protected static StorageClientPool getConnectionPool() throws ClassNotFoundException {
+        HBaseStorageClientPool cp = new HBaseStorageClientPool();
+        cp.activate(ImmutableMap.of("test", (Object) "test"));
+        return cp;
+    }
+
+}
