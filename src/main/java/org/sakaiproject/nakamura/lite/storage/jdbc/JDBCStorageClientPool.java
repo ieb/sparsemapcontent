1901be7d91964f866fa4787a9a6d675e01f7ef53
Ian Boston <ieb@tfd.co.uk>
2011-08-21 01:38:29 +0100

    Removed loggin left behind during testing.

--- src/main/java/org/sakaiproject/nakamura/lite/storage/jdbc/JDBCStorageClientPool.java
+++ src/main/java/org/sakaiproject/nakamura/lite/storage/jdbc/JDBCStorageClientPool.java
@@ -226,7 +226,6 @@ public class JDBCStorageClientPool extends AbstractClientConnectionPool {
 
 
     public Map<String, String> getIndexColumnsNames() {
-        LOGGER.info("Creating Clinet with index Column Names {} ", indexColumnsMap);
         return indexColumnsMap;
     }
 
