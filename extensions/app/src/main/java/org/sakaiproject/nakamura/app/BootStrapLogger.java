2c44800a71b925c0cb811a2829302a583896ccb5
Ian Boston <ieb@tfd.co.uk>
2011-11-23 18:49:51 +1100

    Fixed #130, static content from jars now unpack onto the filesystem see issue for more details

---
+++ extensions/app/src/main/java/org/sakaiproject/nakamura/app/BootStrapLogger.java
@@ -0,0 +1,7 @@
+package org.sakaiproject.nakamura.app;
+
+public interface BootStrapLogger {
+
+	void info(String message, Throwable t);
+
+}
--- extensions/app/src/main/java/org/sakaiproject/nakamura/app/NakamuraMain.java
+++ extensions/app/src/main/java/org/sakaiproject/nakamura/app/NakamuraMain.java
@@ -19,245 +19,273 @@ import java.util.Map;
 
 public class NakamuraMain {
 
-  // The name of the environment variable to consult to find out
-  // about sling.home
-  private static final String ENV_SLING_HOME = "SLING_HOME";
+	// The name of the environment variable to consult to find out
+	// about sling.home
+	private static final String ENV_SLING_HOME = "SLING_HOME";
+	private static String slingHome;
 
-  public static void main(String[] args) throws IOException {
-    checkLaunchDate(args);
-    System.setSecurityManager(null);
-    Main.main(args);
-  }
+	public static void main(String[] args) throws IOException {
+		if (checkLaunchDate(args)) {
+			// new jar check for new content
+			UnBundleStaticContent unBundleStaticContent = new UnBundleStaticContent(
+					new BootStrapLogger() {
 
-  private static void checkLaunchDate(String[] args) throws IOException {
-    // Find the last modified of this jar
-	  try {
-    String resource = NakamuraMain.class.getName().replace('.','/')+".class";
-    URL u = NakamuraMain.class.getClassLoader().getResource(resource);
-    String jarFilePath = u.getFile();
-    jarFilePath = jarFilePath.substring(0,jarFilePath.length()-resource.length()-2);
-    u = new URL(jarFilePath);
-    jarFilePath = u.getFile();
-    File jarFile = new File(jarFilePath);
-    info("Loading from "+jarFile,null);
-    long lastModified = jarFile.lastModified();
+						@Override
+						public void info(String message, Throwable t) {
+							NakamuraMain.info(message, t);
 
-    Map<String, String> parsedArgs = parseCommandLine(args);
-    // Find the last modified when the jar was loaded.
-    String slingHome = getSlingHome(parsedArgs);
-    File slingHomeFile = new File(slingHome);
-    File loaderTimestamp = new File(slingHome, ".lauchpadLastModified");
-    long launchpadLastModified = 0L;
-    if (loaderTimestamp.exists()) {
-      BufferedReader fr = null;
-      try {
-        fr = new BufferedReader(new FileReader(loaderTimestamp));
-        launchpadLastModified = Long.parseLong(fr.readLine());
-      } catch (NumberFormatException e) {
-        e.printStackTrace();
-      } catch (IOException e) {
-        e.printStackTrace();
-      } finally {
-        if (fr != null) {
-          try {
-            fr.close();
-          } catch (IOException e) {
-            e.printStackTrace();
-          }
-        }
-      }
-    } else {
-      info("No loader timestamp ",null);
-    }
+						}
 
-    // if the jar is newer, then delete the bootstrap servialization file that will
-    // cause the contents of the jar to replace the contents on disk.
-    
-    if (launchpadLastModified < lastModified) {
-      File bundleSer = new File(slingHomeFile, "felix/bundle0/bootstrapinstaller.ser");
-      if (bundleSer.exists()) {
-        info("Launcer Jar is newer than runtime image, removing bundle state, jar will reload ",null);
-        bundleSer.delete();
-      } else {
-        info("No runtime, will use contents of launcher jar",null);
-      }
-      slingHomeFile.mkdirs();
-      FileWriter fw = new FileWriter(loaderTimestamp);
-      fw.write(String.valueOf(lastModified));
-      fw.close();
-      fw = null;
-    } else {
-      info("Runtime image, newer than launcher, using runtime image ",null);
-    }
-	  } catch (MalformedURLException e) {
-		  info("Not launching from a jar ",null);
-	  }
+					});
+			unBundleStaticContent.extract(unBundleStaticContent.getClass(),
+					"resources/bundles/", new String[] { "SLING-INF/static",
+							"SLING-INF/home" }, new String[] { "sling/static",
+							slingHome });
+		}
+		System.setSecurityManager(null);
+		Main.main(args);
+	}
 
-  }
+	private static boolean checkLaunchDate(String[] args) throws IOException {
+		// Find the last modified of this jar
+		Map<String, String> parsedArgs = parseCommandLine(args);
+		// Find the last modified when the jar was loaded.
+		slingHome = getSlingHome(parsedArgs);
+		try {
+			String resource = NakamuraMain.class.getName().replace('.', '/')
+					+ ".class";
+			URL u = NakamuraMain.class.getClassLoader().getResource(resource);
+			String jarFilePath = u.getFile();
+			jarFilePath = jarFilePath.substring(0, jarFilePath.length()
+					- resource.length() - 2);
+			u = new URL(jarFilePath);
+			jarFilePath = u.getFile();
+			File jarFile = new File(jarFilePath);
+			info("Loading from " + jarFile, null);
+			long lastModified = jarFile.lastModified();
 
-  /**
-   * Define the sling.home parameter implementing the algorithme defined on the wiki page
-   * to find the setting according to this algorithm:
-   * <ol>
-   * <li>Command line option <code>-c</code></li>
-   * <li>System property <code>sling.home</code></li>
-   * <li>Environment variable <code>SLING_HOME</code></li>
-   * <li>Default value <code>sling</code></li>
-   * </ol>
-   * 
-   * @param args
-   *          The command line arguments
-   * @return The value to use for sling.home
-   */
-  private static String getSlingHome(Map<String, String> commandLine) {
-    String source = null;
+			File slingHomeFile = new File(slingHome);
+			File loaderTimestamp = new File(slingHome, ".lauchpadLastModified");
+			long launchpadLastModified = 0L;
+			if (loaderTimestamp.exists()) {
+				BufferedReader fr = null;
+				try {
+					fr = new BufferedReader(new FileReader(loaderTimestamp));
+					launchpadLastModified = Long.parseLong(fr.readLine());
+				} catch (NumberFormatException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} finally {
+					if (fr != null) {
+						try {
+							fr.close();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			} else {
+				info("No loader timestamp ", null);
+			}
 
-    String slingHome = commandLine.get("c");
-    if (slingHome != null) {
+			// if the jar is newer, then delete the bootstrap servialization
+			// file that will
+			// cause the contents of the jar to replace the contents on disk.
 
-      source = "command line";
+			if (launchpadLastModified < lastModified) {
+				File bundleSer = new File(slingHomeFile,
+						"felix/bundle0/bootstrapinstaller.ser");
+				if (bundleSer.exists()) {
+					info("Launcer Jar is newer than runtime image, removing bundle state, jar will reload ",
+							null);
+					bundleSer.delete();
+				} else {
+					info("No runtime, will use contents of launcher jar", null);
+				}
+				slingHomeFile.mkdirs();
+				FileWriter fw = new FileWriter(loaderTimestamp);
+				fw.write(String.valueOf(lastModified));
+				fw.close();
+				fw = null;
+				return true;
+			} else {
+				info("Runtime image, newer than launcher, using runtime image ",
+						null);
+			}
+		} catch (MalformedURLException e) {
+			info("Not launching from a jar ", null);
+		}
+		return false;
 
-    } else {
+	}
 
-      slingHome = System.getProperty(SharedConstants.SLING_HOME);
-      if (slingHome != null) {
+	/**
+	 * Define the sling.home parameter implementing the algorithme defined on
+	 * the wiki page to find the setting according to this algorithm:
+	 * <ol>
+	 * <li>Command line option <code>-c</code></li>
+	 * <li>System property <code>sling.home</code></li>
+	 * <li>Environment variable <code>SLING_HOME</code></li>
+	 * <li>Default value <code>sling</code></li>
+	 * </ol>
+	 * 
+	 * @param args
+	 *            The command line arguments
+	 * @return The value to use for sling.home
+	 */
+	private static String getSlingHome(Map<String, String> commandLine) {
+		String source = null;
 
-        source = "system property sling.home";
+		String slingHome = commandLine.get("c");
+		if (slingHome != null) {
 
-      } else {
+			source = "command line";
 
-        slingHome = System.getenv(ENV_SLING_HOME);
-        if (slingHome != null) {
+		} else {
 
-          source = "environment variable SLING_HOME";
+			slingHome = System.getProperty(SharedConstants.SLING_HOME);
+			if (slingHome != null) {
 
-        } else {
+				source = "system property sling.home";
 
-          source = "default";
-          slingHome = SharedConstants.SLING_HOME_DEFAULT;
+			} else {
 
-        }
-      }
-    }
+				slingHome = System.getenv(ENV_SLING_HOME);
+				if (slingHome != null) {
 
-    info("Setting sling.home=" + slingHome + " (" + source + ")", null);
-    return slingHome;
-  }
+					source = "environment variable SLING_HOME";
 
-  /**
-   * Parses the command line arguments into a map of strings indexed by strings. This
-   * method suppports single character option names only at the moment. Each pair of an
-   * option name and its value is stored into the map. If a single dash '-' character is
-   * encountered the rest of the command line are interpreted as option names and are
-   * stored in the map unmodified as entries with the same key and value.
-   * <table>
-   * <tr>
-   * <th>Command Line</th>
-   * <th>Mapping</th>
-   * </tr>
-   * <tr>
-   * <td>x</td>
-   * <td>x -> x</td>
-   * </tr>
-   * <tr>
-   * <td>-y z</td>
-   * <td>y -> z</td>
-   * </tr>
-   * <tr>
-   * <td>-yz</td>
-   * <td>y -> z</td>
-   * </tr>
-   * <tr>
-   * <td>-y -z</td>
-   * <td>y -> y, z -> z</td>
-   * </tr>
-   * <tr>
-   * <td>-y x - -z a</td>
-   * <td>y -> x, -z -> -z, a -> a</td>
-   * </tr>
-   * </table>
-   * 
-   * @param args
-   *          The command line to parse
-   * 
-   * @return The map of command line options and their values
-   */
-  static Map<String, String> parseCommandLine(String[] args) {
-    Map<String, String> commandLine = new HashMap<String, String>();
-    boolean readUnparsed = false;
-    for (int argc = 0; args != null && argc < args.length; argc++) {
-      String arg = args[argc];
+				} else {
 
-      if (readUnparsed) {
-        commandLine.put(arg, arg);
-      } else if (arg.startsWith("-")) {
-        if (arg.length() == 1) {
-          readUnparsed = true;
-        } else {
-          String key = String.valueOf(arg.charAt(1));
-          if (arg.length() > 2) {
-            commandLine.put(key, arg.substring(2));
-          } else {
-            argc++;
-            if (argc < args.length
-                && (args[argc].equals("-") || !args[argc].startsWith("-"))) {
-              commandLine.put(key, args[argc]);
-            } else {
-              commandLine.put(key, key);
-              argc--;
-            }
-          }
-        }
-      } else {
-        commandLine.put(arg, arg);
-      }
-    }
-    return commandLine;
-  }
-  
-  // ---------- logging
+					source = "default";
+					slingHome = SharedConstants.SLING_HOME_DEFAULT;
 
-  // emit an informational message to standard out
-  static void info(String message, Throwable t) {
-      log(System.out, "*INFO*", message, t);
-  }
+				}
+			}
+		}
 
-  // emit an error message to standard err
-  static void error(String message, Throwable t) {
-      log(System.err, "*ERROR*", message, t);
-  }
+		info("Setting sling.home=" + slingHome + " (" + source + ")", null);
+		return slingHome;
+	}
 
-  private static final DateFormat fmt = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss.SSS ");
+	/**
+	 * Parses the command line arguments into a map of strings indexed by
+	 * strings. This method suppports single character option names only at the
+	 * moment. Each pair of an option name and its value is stored into the map.
+	 * If a single dash '-' character is encountered the rest of the command
+	 * line are interpreted as option names and are stored in the map unmodified
+	 * as entries with the same key and value.
+	 * <table>
+	 * <tr>
+	 * <th>Command Line</th>
+	 * <th>Mapping</th>
+	 * </tr>
+	 * <tr>
+	 * <td>x</td>
+	 * <td>x -> x</td>
+	 * </tr>
+	 * <tr>
+	 * <td>-y z</td>
+	 * <td>y -> z</td>
+	 * </tr>
+	 * <tr>
+	 * <td>-yz</td>
+	 * <td>y -> z</td>
+	 * </tr>
+	 * <tr>
+	 * <td>-y -z</td>
+	 * <td>y -> y, z -> z</td>
+	 * </tr>
+	 * <tr>
+	 * <td>-y x - -z a</td>
+	 * <td>y -> x, -z -> -z, a -> a</td>
+	 * </tr>
+	 * </table>
+	 * 
+	 * @param args
+	 *            The command line to parse
+	 * 
+	 * @return The map of command line options and their values
+	 */
+	static Map<String, String> parseCommandLine(String[] args) {
+		Map<String, String> commandLine = new HashMap<String, String>();
+		boolean readUnparsed = false;
+		for (int argc = 0; args != null && argc < args.length; argc++) {
+			String arg = args[argc];
 
-  // helper method to format the message on the correct output channel
-  // the throwable if not-null is also prefixed line by line with the prefix
-  private static void log(PrintStream out, String prefix, String message,
-          Throwable t) {
+			if (readUnparsed) {
+				commandLine.put(arg, arg);
+			} else if (arg.startsWith("-")) {
+				if (arg.length() == 1) {
+					readUnparsed = true;
+				} else {
+					String key = String.valueOf(arg.charAt(1));
+					if (arg.length() > 2) {
+						commandLine.put(key, arg.substring(2));
+					} else {
+						argc++;
+						if (argc < args.length
+								&& (args[argc].equals("-") || !args[argc]
+										.startsWith("-"))) {
+							commandLine.put(key, args[argc]);
+						} else {
+							commandLine.put(key, key);
+							argc--;
+						}
+					}
+				}
+			} else {
+				commandLine.put(arg, arg);
+			}
+		}
+		return commandLine;
+	}
 
-      final StringBuilder linePrefixBuilder = new StringBuilder();
-      synchronized (fmt) {
-          linePrefixBuilder.append(fmt.format(new Date()));
-      }
-      linePrefixBuilder.append(prefix);
-      linePrefixBuilder.append(" [");
-      linePrefixBuilder.append(Thread.currentThread().getName());
-      linePrefixBuilder.append("] ");
-      final String linePrefix = linePrefixBuilder.toString();
+	// ---------- logging
 
-      out.print(linePrefix);
-      out.println(message);
-      if (t != null) {
-          t.printStackTrace(new PrintStream(out) {
-              @Override
-              public void println(String x) {
-                  synchronized (this) {
-                      print(linePrefix);
-                      super.println(x);
-                      flush();
-                  }
-              }
-          });
-      }
-  }
+	// emit an informational message to standard out
+	static void info(String message, Throwable t) {
+		log(System.out, "*INFO*", message, t);
+	}
+
+	// emit an error message to standard err
+	static void error(String message, Throwable t) {
+		log(System.err, "*ERROR*", message, t);
+	}
+
+	private static final DateFormat fmt = new SimpleDateFormat(
+			"dd.MM.yyyy HH:mm:ss.SSS ");
+
+	// helper method to format the message on the correct output channel
+	// the throwable if not-null is also prefixed line by line with the prefix
+	private static void log(PrintStream out, String prefix, String message,
+			Throwable t) {
+
+		final StringBuilder linePrefixBuilder = new StringBuilder();
+		synchronized (fmt) {
+			linePrefixBuilder.append(fmt.format(new Date()));
+		}
+		linePrefixBuilder.append(prefix);
+		linePrefixBuilder.append(" [");
+		linePrefixBuilder.append(Thread.currentThread().getName());
+		linePrefixBuilder.append("] ");
+		final String linePrefix = linePrefixBuilder.toString();
+
+		out.print(linePrefix);
+		out.println(message);
+		if (t != null) {
+			t.printStackTrace(new PrintStream(out) {
+				@Override
+				public void println(String x) {
+					synchronized (this) {
+						print(linePrefix);
+						super.println(x);
+						flush();
+					}
+				}
+			});
+		}
+	}
 
 }
