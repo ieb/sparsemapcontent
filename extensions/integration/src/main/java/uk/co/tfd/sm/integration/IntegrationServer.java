8e55ea6aa68016e43ad4aa03956ce205a0a31cff
Ian Boston <ieb@tfd.co.uk>
2012-02-09 11:01:23 +1100

    Fixed #160, no major code changes required.

--- extensions/integration/src/main/java/uk/co/tfd/sm/integration/IntegrationServer.java
+++ extensions/integration/src/main/java/uk/co/tfd/sm/integration/IntegrationServer.java
@@ -1,15 +1,12 @@
 package uk.co.tfd.sm.integration;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.ConnectException;
 import java.net.MalformedURLException;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.sakaiproject.nakamura.app.NakamuraMain;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,15 +23,18 @@ public class IntegrationServer {
 			if (isStarted(0)) {
 				LOGGER.info("App already Running on port 8080");
 			} else {
+				throw new IllegalStateException(
+						"Please start integration test server on port 8080");
+				/*
+				 * Removed to prevent a cyclic dependency and build order issues.
 				File f = new File("target/integrationserver");
 				FileUtils.deleteDirectory(f.getAbsoluteFile());
 				System.setProperty("sling.home", f.getAbsolutePath());
 				NakamuraMain.main(new String[] { "-f", "target/integrationserver.log" });
 				LOGGER.info("Started App");
 				if (!isStarted(60000)) {
-					throw new IllegalStateException(
-							"Failed to start an integration test server on port 8080");
 				}
+				*/
 			}
 			started = true;
 		}
