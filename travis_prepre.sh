34a44b153b31c5bd97660b9e4921c4f65887e092
Ian Boston <ieb@tfd.co.uk>
2012-10-01 09:21:21 +1000

    Error in preload script.

--- travis_prepre.sh
+++ travis_prepre.sh
@@ -6,6 +6,6 @@ curl http://www.ettrema.com/maven2/com/ettrema/milton-servlet/1.8.1.4/milton-ser
 curl http://www.ettrema.com/maven2/com/ettrema/milton-servlet/1.8.1.4/milton-servlet-1.8.1.4.pom > /tmp/milton-servlet-1.8.1.4.pom
 mvn install:install-file -DgroupId=com.ettrema -DartifactId=milton-api -Dpackaging=pom -Dversion=1.8.1.4 -Dfile=/tmp/milton-api-1.8.1.4.pom
 mvn install:install-file -DgroupId=com.ettrema -DartifactId=milton-api -Dpackaging=jar -Dversion=1.8.1.4 -Dfile=/tmp/milton-api-1.8.1.4.jar
-mvn install:install-file -DgroupId=com.ettrema -DartifactId=milton-servlet -Dpackaging=pom -Dversion=1.8.1.4 -Dfile=/tmp/milton-api-1.8.1.4.pom
-mvn install:install-file -DgroupId=com.ettrema -DartifactId=milton-servlet -Dpackaging=jar -Dversion=1.8.1.4 -Dfile=/tmp/milton-api-1.8.1.4.jar
+mvn install:install-file -DgroupId=com.ettrema -DartifactId=milton-servlet -Dpackaging=pom -Dversion=1.8.1.4 -Dfile=/tmp/milton-servlet-1.8.1.4.pom
+mvn install:install-file -DgroupId=com.ettrema -DartifactId=milton-servlet -Dpackaging=jar -Dversion=1.8.1.4 -Dfile=/tmp/milton-servlet-1.8.1.4.jar
 
